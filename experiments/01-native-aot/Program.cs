753a437d2e61777b08c900add54616208829c515
cm-collins <dev-collins@outlook.com>
2026-09-07 19:30:27 +0000

    Add Native AOT experiment

---
+++ experiments/01-native-aot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("=== .NET Native AOT Experiment ===");
+Console.WriteLine();
+
+var stopwatch = Stopwatch.StartNew();
+
+const int iterations = 10_000_000;
+
+long result = 0;
+
+for (var i = 0; i < iterations; i++)
+{
+    result += Calculate(i);
+}
+
+stopwatch.Stop();
+
+Console.WriteLine($"Result: {result}");
+Console.WriteLine($"Execution time: {stopwatch.ElapsedMilliseconds} ms");
+
+static long Calculate(int value)
+{
+    return (long)value * value;
+}
